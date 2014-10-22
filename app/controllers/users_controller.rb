5fad2bc9130353af8874b171aaa444dc3b99f17a
Satinderjit Grewal <grewalsatinder@yahoo.com>
2014-10-22 14:01:47 -0700

    changed userSignup works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
  def create
     @user = User.new(user_params)
     if @user.save
+       
     	flash[:success] = "Welcome to Travelio!"
     	   redirect_to @user
       # Handle a successful save.
